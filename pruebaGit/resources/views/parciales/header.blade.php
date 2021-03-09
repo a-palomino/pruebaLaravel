477a43a5925d1dd4408814109bf797cfec9be3ba
a-palomino <a.palomino@sapalomera.cat>
2021-03-09 11:20:36 +0100

    Añadido bootstrap

---
+++ pruebaGit/resources/views/parciales/header.blade.php
@@ -0,0 +1,6 @@
+<div class="header">
+
+    <!-- cambiar para poner ruta relativa -->
+    <div><a href="<?php echo url('')?>"><img src="<?php echo url('img/prueba.png')?>" width="100" height="100"></a></div>
+    <div><a href=''>Otra Redireccion</a></div>
+</div>
--- pruebaGit/resources/views/plantilla.blade.php
+++ pruebaGit/resources/views/plantilla.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+ 
 </head>
 <body>
     <!-- Donde irá el header -->
